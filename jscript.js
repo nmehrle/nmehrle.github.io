11e7a4402cc387c9c0e7d50a7f594c693f411c24
Nicholas Mehrle <nmehrle@gmail.com>
2015-04-29 16:40:10 +0200

    .

--- jscript.js
+++ jscript.js
@@ -15,31 +15,34 @@ $(document).ready(function() {
 	});
 
 	var lines = ["#header","#work","#school","#play"];
-	var heights = [230,180,180,180];
-	var fullHeight = 180;
+	var subHeight = 135;
+	var mainHeight = 300;
+	var heights = [mainHeight,subHeight,subHeight,subHeight];
+	var tops = [0,mainHeight,mainHeight+subHeight,mainHeight+2*subHeight];
 	var delta = 15;
+	animationTime=150;
 
-	$(".line").height(fullHeight+"px");
 
-
-	for (var i = 0; i < lines.length; i++) {
-		$(lines[i]).css("top",i*fullHeight+"px");
-	};
-
-	jQuery.each(lines, function(i,val) {
+	$.each(lines, function(i,val) {
+		$(val).height(heights[i]+"px");
+		$(val).css("top",tops[i]+"px");
 		if(i!=0) {
 			$(val).hover(function() {
-				$(lines[i-1]).animate({height:heights[i-1]-delta+"px"},{duration:200,queue:false});
-				$(val).animate({height:fullHeight+2*delta+"px"},{duration:200,queue:false});
-				$(val).animate({top:(fullHeight*i-delta)+"px"},{duration:200,queue:false});
-				$(lines[i+1]).animate({height:fullHeight-delta+"px"},{duration:200,queue:false});
-				$(lines[i+1]).animate({top:(fullHeight*(i+1)+delta)+"px"},{duration:200,queue:false});
+				$(lines[i-1]).animate({height:heights[i-1]-delta+"px"},{duration:animationTime,queue:false});
+				$(val).animate({height:heights[i]+2*delta+"px"},{duration:animationTime,queue:false});
+				$(val).animate({top:(tops[i]-delta)+"px"},{duration:animationTime,queue:false});
+				if(i!=3) {
+					$(lines[i+1]).animate({height:heights[i+1]-delta+"px"},{duration:animationTime,queue:false});
+					$(lines[i+1]).animate({top:(tops[i+1]+delta)+"px"},{duration:animationTime,queue:false});
+				}
 			},function() {
-				$(lines[i-1]).animate({height:heights[i-1]+"px"},{duration:200,queue:false});
-				$(val).animate({height:fullHeight+"px"},{duration:200,queue:false});
-				$(val).animate({top:(fullHeight*i)+"px"},{duration:200,queue:false});
-				$(lines[i+1]).animate({height:fullHeight+"px"},{duration:200,queue:false});
-				$(lines[i+1]).animate({top:(fullHeight*(i+1))+"px"},{duration:200,queue:false});
+				$(lines[i-1]).animate({height:heights[i-1]+"px"},{duration:animationTime,queue:false});
+				$(val).animate({height:heights[i]+"px"},{duration:animationTime,queue:false});
+				$(val).animate({top:tops[i]+"px"},{duration:animationTime,queue:false});
+				if(i!=3) {
+					$(lines[i+1]).animate({height:heights[i+1]+"px"},{duration:animationTime,queue:false});
+					$(lines[i+1]).animate({top:tops[i+1]+"px"},{duration:animationTime,queue:false});
+				}
 			});
 		}
 	});
