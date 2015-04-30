64d8e114557b6bd21ba0b14334a0cff64b2bbfa4
Nicholas Mehrle <nmehrle@gmail.com>
2015-04-30 19:37:32 +0200

    working on pull out of bars

--- jscript.js
+++ jscript.js
@@ -17,10 +17,11 @@ $(document).ready(function() {
 	var lines = ["#header","#work","#school","#play"];
 	var subHeight = 135;
 	var mainHeight = 300;
-	var overlap =20;
+	var overlap =0;
 	var heights = [mainHeight,subHeight+overlap,subHeight,subHeight+overlap];
 	var tops = [0,mainHeight,mainHeight+subHeight,mainHeight+2*subHeight-overlap];
 	var delta = 15;
+	var zmax =1;
 	animationTime=200;
 
 	$('.textSpace').height(overlap+"px");
@@ -30,21 +31,37 @@ $(document).ready(function() {
 		$(val).css("top",tops[i]+"px");
 		if(i!=0) {
 			$(val).hover(function() {
-				$(lines[i-1]).animate({height:heights[i-1]-delta+"px"},{duration:animationTime,queue:false});
+				$(val).css("z-index",zmax);
+				zmax++;
+				console.log(zmax);
+				//$(lines[i-1]).animate({height:heights[i-1]-delta+"px"},{duration:animationTime,queue:false});
 				$(val).animate({height:heights[i]+2*delta+"px"},{duration:animationTime,queue:false});
 				$(val).animate({top:(tops[i]-delta)+"px"},{duration:animationTime,queue:false});
+				
+				$(val).animate({width:"105%"},{duration:animationTime,queue:false});
+				$(val).animate({left:"-2.5%"},{duration:animationTime,queue:false});
+
 				if(i!=3) {
-					$(lines[i+1]).animate({height:heights[i+1]-delta+"px"},{duration:animationTime,queue:false});
-					$(lines[i+1]).animate({top:(tops[i+1]+delta)+"px"},{duration:animationTime,queue:false});
+					//$(lines[i+1]).animate({height:heights[i+1]-delta+"px"},{duration:animationTime,queue:false});
+					//$(lines[i+1]).animate({top:(tops[i+1]+delta)+"px"},{duration:animationTime,queue:false});
 				}
 			},function() {
-				$(lines[i-1]).animate({height:heights[i-1]+"px"},{duration:animationTime,queue:false});
+				//$(lines[i-1]).animate({height:heights[i-1]+"px"},{duration:animationTime,queue:false});
 				$(val).animate({height:heights[i]+"px"},{duration:animationTime,queue:false});
 				$(val).animate({top:tops[i]+"px"},{duration:animationTime,queue:false});
+
+				$(val).animate({width:"100%"},{duration:animationTime,queue:false});
+				$(val).animate({left:"0"},{duration:animationTime,queue:false});
+
 				if(i!=3) {
-					$(lines[i+1]).animate({height:heights[i+1]+"px"},{duration:animationTime,queue:false});
-					$(lines[i+1]).animate({top:tops[i+1]+"px"},{duration:animationTime,queue:false});
+					//$(lines[i+1]).animate({height:heights[i+1]+"px"},{duration:animationTime,queue:false});
+					//$(lines[i+1]).animate({top:tops[i+1]+"px"},{duration:animationTime,queue:false});
 				}
+				setTimeout(function() {
+					$(val).css("z-index",0);
+					zmax--;
+					console.log(zmax);
+				},animationTime);
 			});
 		}
 	});
