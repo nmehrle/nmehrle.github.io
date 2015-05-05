8ac638887cfc91971bc30f71b4b87650970d2bcf
Nicholas Mehrle <nmehrle@gmail.com>
2015-05-05 16:49:28 +0200

    MenuBar animations and styling

--- jscript.js
+++ jscript.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
 	var bars = ["#work","#school","#play"];
 	var height = 135;
 	var headHeight=300;
+	var headTop = 15;
 	var tops = [headHeight,headHeight+height,headHeight+2*height];
 	animationTime=200;
 
-	$("#header").height(headHeight+"px");
+	$("#header").height(headHeight+"px").css("top",headTop+"px");
 
 	$.each(bars, function(i,val) {
 		$(val).height(height+"px");
-		$(val).css("top",tops[i]+15+"px");
+		$(val).css("top",tops[i]+headTop+"px");
 		$(val).hover(function() {
 			$(val).velocity({width:"104%"},{duration:animationTime,queue:false});
 			$(val).velocity({left:"-2%"},{duration:animationTime,queue:false});
